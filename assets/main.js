c37189ce121873423202a216195fb17a0cbf6dd2
Opal <chuan255@newschool.edu>
2025-03-31 14:15:26 -0400

    Set constant value for each

--- assets/main.js
+++ assets/main.js
@@ -5,3 +5,10 @@
 		const wakeupTimeStr = document.getElementById('wakeupTime').value;
 		const snoozeCount = parseInt(document.getElementById('snoozeCount').value);
 		const sleepCycles = parseInt(document.getElementById('sleepCycles').value);
+
+		// Set Constants value for each 
+		const CYCLE_MINUTES = 90;
+		const FALL_ASLEEP_MINUTES = 15;
+		const SNOOZE_MINUTES = 9;
+
+		
