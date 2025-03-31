058cec491c4b8aa265d32d9627cba461027f4562
Opal <chuan255@newschool.edu>
2025-03-31 14:13:53 -0400

    Remove custom cycle, Add more sleep cycle options

--- assets/main.js
+++ assets/main.js
@@ -1,25 +1,7 @@
-	
-	// show and hide sleep cycle based on input value
-	document.getElementById('sleepCycles').addEventListener('change', function() {
-	document.getElementById('customCyclesGroup').style.display = 
-	this.value === 'custom' ? 'block' : 'none';
-	});
 
+	//Snooze Calculator Function Javascript//
 	function calculateSleepTimes() {
 		// Get input values
 		const wakeupTimeStr = document.getElementById('wakeupTime').value;
 		const snoozeCount = parseInt(document.getElementById('snoozeCount').value);
-
-		// Get sleep cycles
-		let sleepCycles;
-		const sleepCyclesSelect = document.getElementById('sleepCycles').value;
-		if (sleepCyclesSelect === 'custom') {
-			sleepCycles = parseInt(document.getElementById('customCycles').value);
-		} else {
-			sleepCycles = parseInt(sleepCyclesSelect);
-	}
-		
-		// Set Constants value/time for each opt
-			const CYCLE_MINUTES = 90;
-			const FALL_ASLEEP_MINUTES = 15;
-			const SNOOZE_MINUTES = 9;
+		const sleepCycles = parseInt(document.getElementById('sleepCycles').value);
