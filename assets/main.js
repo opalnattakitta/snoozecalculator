212c399d73e3ea0613bb6c2a3d325754a4e7cf1e
Opal <chuan255@newschool.edu>
2025-04-21 16:23:27 -0400

    Add '?' button for more explaination

--- assets/main.js
+++ assets/main.js
@@ -106,5 +106,20 @@
 				entry.classList.remove('show');
 			});
 		}
+
+		function openModal() {
+			document.getElementById("infoModal").style.display = "block";
+		}
+		  
+		function closeModal() {
+			document.getElementById("infoModal").style.display = "none";
+		}
+		
+		window.onclick = function(event) {
+			const modal = document.getElementById("infoModal");
+			if (event.target == modal) {
+				modal.style.display = "none";
+			}
+		};
 	});
 	
